65ff267e9700e7f982e6a5d83cf1d764cdcc697d
khairilrahman55 <khairilrahman55@gmail.com>
2024-06-19 11:22:52 +0000

    useMemo

---
+++ src/hooks/UseMemo.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useMemo, useState } from 'react';
+//untuk mempercepat program yg lambat
+
+function getKuadrat(num) {
+  console.log('fungsi kuadrat');
+  for (let i = 0; i < 2000000000; i++) {}
+  return num * num;
+}
+const UseMemo = () => {
+    const [count, setCount] = useState(0);
+    const [redButton, setRedButton] = useState(false);
+    
+  const kuadrat = useMemo(() => {
+    return getKuadrat(count);
+  }, [count]);//dep: sebagai, hanya count yg akan mendapatkan delay saat re render  
+    //menimbulkan red button akan di render ulang, maka kita bisa melakukan memo juga untuk pada display
+  const display = useMemo(() => {
+    return {
+      text: redButton
+        ? 'Click untuk membuat button blue'
+        : 'Click untuk membuat button red',
+      color: `${redButton ? 'red' : ''}`,
+    };
+  }, [redButton]);
+  useEffect(() => {
+    console.log('display dibuat ulang');
+  }, [display]);
+  return (
+    <div className="container">
+      <div className="hooks">useMemo</div>
+      <div className="content">
+        <button
+          className={`${redButton ? 'red' : ''}`}
+          onClick={() => setCount((count) => count - 1)}>
+          decrement
+        </button>
+        <div>{count}</div>
+        <button
+          className={`${redButton ? 'red' : ''}`}
+          onClick={() => setCount((count) => count + 1)}>
+          increment
+        </button>
+      </div>
+      <button
+        className={display.color}
+        onClick={() => setRedButton((color) => !color)}>
+        {display.text}
+      </button>
+      <div>Hasil dari count x count = {kuadrat}</div>
+    </div>
+  );
+};
+
+export default UseMemo;
